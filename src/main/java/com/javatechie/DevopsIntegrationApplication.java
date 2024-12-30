f79a4e000655e43d0ec041c10d0494616d1e0770
Vijayakumar Thangavel <vijayakumar.thangavel@gmail.com>
2024-12-30 15:48:17 +0530

    Modified Code

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -2,12 +2,21 @@ package com.javatechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsIntegrationApplication.class, args);
-	}
+    @GetMapping
+    public String message() {
+
+         return "SringBoot Hello World";
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
 
 }
--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DevopsIntegrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
